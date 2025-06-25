3c9e80c32de161aa05e3455dc716cb2b0ff2fa6a
Skywalker <github@pengatom.com>
2025-06-25 21:57:21 +0200

    Small script for testing left hand

--- src/hros5_description/launch/test_dynamixel.launch.py
+++ src/hros5_description/launch/test_dynamixel.launch.py
@@ -1,33 +1,50 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.substitutions import FindPackageShare
 import os
 
 def generate_launch_description():
     pkg_share = FindPackageShare("hros5_description").find("hros5_description")
 
+    # Expand the minimal hands-only xacro
+    robot_description_content = Command([
+        "xacro ",
+        os.path.join(pkg_share, "urdf", "display_hands.urdf.xacro")
+    ])
+    robot_description = {"robot_description": robot_description_content}
+
     return LaunchDescription([
         DeclareLaunchArgument(
-            "urdf_file",
-            default_value=os.path.join(pkg_share, "urdf", "head", "display_head.urdf.xacro"),
-            description="Path to URDF/Xacro file",
-        ),
-        DeclareLaunchArgument(
             "dynamixel_config",
-            default_value=os.path.join(pkg_share, "config", "dynamixel.yaml"),
+            default_value=os.path.join(
+                FindPackageShare("hros5_control").find("hros5_control"),
+                "config", "dynamixel.yaml"
+            ),
             description="YAML config for dynamixel_hardware_interface",
         ),
+
+        # Publish robot description
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            name="robot_state_publisher",
+            output="screen",
+            parameters=[robot_description],
+        ),
+
+        # Launch ros2_control with just the YAML
         Node(
             package="controller_manager",
             executable="ros2_control_node",
-            parameters=[
-                LaunchConfiguration("urdf_file"),
-                LaunchConfiguration("dynamixel_config")
-            ],
+            parameters=[{
+                LaunchConfiguration("dynamixel_config"): LaunchConfiguration("dynamixel_config")
+            }],
             output="screen"
         ),
+
+        # Spawn joint_state_broadcaster
         Node(
             package="controller_manager",
             executable="spawner",
@@ -35,4 +52,3 @@ def generate_launch_description():
             output="screen"
         )
     ])
-
