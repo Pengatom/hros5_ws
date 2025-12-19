2cf5b6a0572bb02cf39a6007af7839a6e75e941c
Palpatine <github@pengatom.com>
2025-12-19 13:05:49 +0100

    gazebo: Add a simulated IMU to the robot for balance and state estimation in Gazebo.

--- src/hros5_gazebo/launch/hros5_gazebo.launch.py
+++ src/hros5_gazebo/launch/hros5_gazebo.launch.py
@@ -183,7 +183,8 @@ def generate_launch_description():
             executable='parameter_bridge',
             name='gz_clock_bridge',
             arguments=[
-                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'
+                '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+                '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU'
             ],
             output='screen'
         ),
